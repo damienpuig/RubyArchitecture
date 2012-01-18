02dd0d36a607409fc557246e5419ba4c523799d8
Damien PUIG <puigfb@gmail.com>
2012-01-18 16:41:37 +0000

    Troisieme commit et c'est parti les gars pour le projet de folie
    Voici l'arborescence sans active record pour pouvoir faire du mongo ( mongo n'est pas installé, a suivre...)
    
    Signed-off-by: Damien PUIG <puigfb@gmail.com>

---
+++ RubyArchi/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyArchi::Application.config.secret_token = '67891793accccdf94f687c0c79d531c475eef2c082640da05f1a8d2c5114f43ed993ad58827bf5356002a25642f05d06bf4772536c524c9b4860a9cfb86da942'
